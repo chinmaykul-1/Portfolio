e06ba9bfa3c195ce55532e579ebad7dbb188c03f
chinmaykul-1 <chinmaykulkarni81@gmail.com>
2025-04-21 00:19:53 +0530

    Update: Resume

--- src/components/presentation/logos.tsx
+++ src/components/presentation/logos.tsx
@@ -5,7 +5,7 @@ export function LogoSectionOne() {
     /* From Uiverse.io by nathAd17 with modifications for Instagram-like gradient effect */
     <a
       className="py-4 px-4"
-      href="https://drive.google.com/file/d/1i3i7YzuZUzIrdHALMIsvXW5Eolp8H0n3/view"
+      href="https://drive.google.com/file/d/1itcqeEKd1TM9B8bjyf9VJx561yqRZZQQ/view"
       target="_blank"
       rel="noopener noreferrer"
     >
